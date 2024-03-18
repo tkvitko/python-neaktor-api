13b2dd911bbb62eafc2dbe5b2bc96c2e21be2e3c
Taras Kvitko <tkvitko@gmail.com>
2024-03-18 18:40:19 +0300

    Methods to create tasks and comments

--- example.py
+++ example.py
@@ -2,9 +2,23 @@ from neaktor.api import NeaktorApiClient
 
 
 if __name__ == '__main__':
-    my_public_key = 'my_public_key'
+    my_public_key = 'test'
     api = NeaktorApiClient(public_key=my_public_key)
+
     my_tasks = api.get_tasks()
     print(len(my_tasks))
+
     my_users = api.get_users()
     print(len(my_users))
+
+    my_task_models = api.get_task_modes()
+    print(my_task_models)
+
+    new_task = api.add_task(model_id='my_model_id',
+                            fields={'subject': 'my new task'},
+                            assignee_id=1234)
+    print(new_task)
+
+    new_comment = api.add_comment(task_id='my_task_id',
+                                  text='my comment')
+    print(new_comment)
--- neaktor/api.py
+++ neaktor/api.py
@@ -15,15 +15,14 @@ class NoAuthParam(Exception):
 
 
 class NeaktorApiClient:
-
     """
     Main class of API wrapper
     """
 
     def __init__(self,
-                 public_key:    str = None,
-                 access_token:  str = None,
-                 timeout:       int = 60):
+                 public_key: str = None,
+                 access_token: str = None,
+                 timeout: int = 60):
 
         # base params:
         self.base_url = 'https://api.neaktor.com/'
@@ -51,10 +50,18 @@ class NeaktorApiClient:
         :return:
         """
 
+        response = None
         if method == 'GET':
             response = requests.get(url=self.base_url + api_path,
                                     headers=self.request_headers,
                                     timeout=self.timeout)
+
+        elif method == 'POST':
+            response = requests.post(url=self.base_url + api_path,
+                                     headers=self.request_headers,
+                                     timeout=self.timeout,
+                                     json=body)
+        if response is not None:
             return response.json()
 
     def _get_objects(self,
@@ -90,9 +97,51 @@ class NeaktorApiClient:
 
         return data
 
+    def _add_object(self,
+                    api_path: str,
+                    parent_id: str,
+                    fields: dict = None,
+                    assignee_id: int = None,
+                    assignee_type: str = 'USER',
+                    text: str = None,
+                    ) -> Union[dict, None]:
+        """
+        Base request for any neaktor object create
+        :param api_path: api path
+        :param parent_id: id of the parent entity
+        :param fields: list of entity fields
+        :param assignee_id: id of assignee
+        :param assignee_type: type of assignee ('USER', 'GROUP')
+        :param text: text for comment
+        :return: created object
+        """
+
+        api_path += f'/{parent_id}'
+        body = dict()
+
+        if assignee_id is not None:
+            body['assignee'] = {'id': assignee_id,
+                                'type': assignee_type}
+
+        if text is not None:
+            body['text'] = text
+
+        if fields is not None:
+            body['fields'] = [{
+                'id': field[0],
+                'value': field[1],
+                'state': 'editable'} for field in fields.items()]
+
+        result = self._base_api_request(api_path=api_path,
+                                        body=body,
+                                        method='POST')
+
+        return result
+
     def get_tasks(self,
                   page_size: int = DEFAULT_PAGE_SIZE,
-                  **params):
+                  **params
+                  ) -> List[dict]:
         """
         https://developers.neaktor.com/#/tasks
         :param page_size: objects number per page
@@ -106,9 +155,27 @@ class NeaktorApiClient:
                                     params=params)
         return objects
 
+    def get_task_modes(self,
+                       page_size: int = DEFAULT_PAGE_SIZE,
+                       **params
+                       ) -> List[dict]:
+        """
+        https://developers.neaktor.com/#/taskmodels
+        :param page_size: objects number per page
+        :param params: any get parameters
+        :return: tasks list
+        """
+
+        api_path = 'v1/taskmodels'
+        objects = self._get_objects(api_path=api_path,
+                                    page_size=page_size,
+                                    params=params)
+        return objects
+
     def get_users(self,
                   page_size: int = DEFAULT_PAGE_SIZE,
-                  **params):
+                  **params
+                  ) -> List[dict]:
         """
         https://developers.neaktor.com/#/users
         :param page_size: objects number per page
@@ -121,3 +188,43 @@ class NeaktorApiClient:
                                     page_size=page_size,
                                     params=params)
         return objects
+
+    def add_task(self,
+                 model_id: str,
+                 fields: dict = None,
+                 assignee_id: int = None,
+                 assignee_type: str = 'USER'
+                 ) -> Union[dict | None]:
+        """
+        https://developers.neaktor.com/#/tasks/id/create
+        :param model_id: id of model to create task to
+        :param fields: dict of task fields
+        :param assignee_id: id of assignee
+        :param assignee_type: type of assignee ('USER', 'GROUP')
+        :return: created task
+        """
+
+        result = self._add_object(api_path='v1/tasks',
+                                  parent_id=model_id,
+                                  fields=fields,
+                                  assignee_id=assignee_id,
+                                  assignee_type=assignee_type)
+
+        return result
+
+    def add_comment(self,
+                    task_id: str,
+                    text: str
+                    ) -> Union[dict | None]:
+        """
+        https://developers.neaktor.com/#/comments/create
+        :param task_id: if of task to add comment to
+        :param text: text to add comment with
+        :return:
+        """
+
+        result = self._add_object(api_path='v1/comments',
+                                  parent_id=task_id,
+                                  text=text)
+
+        return result
